741a4ebdf0811f02d265bf8717bb53b04678af87
Artur Leite <arturleite164@gmail.com>
2024-12-07 15:37:14 +0000

    Added fish as default shell

---
+++ .config/fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    fish_add_path $HOME/bin
+    fish_add_path $HOME/.local/bin
+    fish_add_path /opt/homebrew/bin
+
+    zoxide init fish --cmd cd | source
+
+    theme_gruvbox dark hard
+end
---
+++ .config/fish/functions/config.fish
@@ -0,0 +1,4 @@
+function config --wraps='/usr/bin/git --git-dir=/Users/arturleitework/.dotfiles --work-tree=/Users/arturleitework' --description 'alias config /usr/bin/git --git-dir=/Users/arturleitework/.dotfiles --work-tree=/Users/arturleitework'
+  /usr/bin/git --git-dir=/Users/arturleitework/.dotfiles --work-tree=/Users/arturleitework $argv
+        
+end
