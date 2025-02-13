a1b67f6c24634a63fe4ec8d09e42664307f57e8b
Artur Leite <arturleite164@gmail.com>
2025-02-13 10:04:01 +0000

    Added fish and zed updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,13 @@ if status is-interactive
     fish_add_path $HOME/bin
     fish_add_path $HOME/.local/bin
     fish_add_path /opt/homebrew/bin
+    fish_add_path $HOME/.emacs.d/bin
+    fish_add_path $HOME/dev/roc
+    fish_add_path /opt/homebrew/opt/llvm/bin
+    set -gx CC /opt/homebrew/opt/llvm/bin/clang
+    set -gx CXX /opt/homebrew/opt/llvm/bin/clang++
+    alias gcc /opt/homebrew/opt/llvm/bin/clang
+    alias g++ /opt/homebrew/opt/llvm/bin/clang++
     zoxide init fish --cmd cd | source
 
     theme_gruvbox dark hard
