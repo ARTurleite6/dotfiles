709c141b3723339dc812f29e34321774ab3d00b1
Artur Leite <arturleite164@gmail.com>
2025-05-25 13:18:08 +0100

    Removed fish configuration, going back to zsh

--- .config/fish/functions/config.fish
+++
@@ -1,4 +0,0 @@
-function config --wraps='/usr/bin/git --git-dir=/Users/arturleitework/.dotfiles --work-tree=/Users/arturleitework' --description 'alias config /usr/bin/git --git-dir=/Users/arturleitework/.dotfiles --work-tree=/Users/arturleitework'
-  /usr/bin/git --git-dir=/Users/arturleitework/.dotfiles --work-tree=/Users/arturleitework $argv
-        
-end
